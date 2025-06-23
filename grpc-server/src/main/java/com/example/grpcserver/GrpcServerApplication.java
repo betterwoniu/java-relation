db52890059f9aed197d6e6b1f501c68e987c8074
吕本诺 <lv199618@163.com>
2025-06-23 17:19:49 +0800

    grpc link

---
+++ grpc-server/src/main/java/com/example/grpcserver/GrpcServerApplication.java
@@ -0,0 +1,16 @@
+package com.example.grpcserver;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.example.grpcserver.mapper")
+
+public class GrpcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcServerApplication.class, args);
+    }
+
+}
